1f0fb83ae78dfe43c048d72c43da59511df92110
Chang WooMin <changwoomin426@gmail.com>
2024-12-26 13:26:01 +0900

    Create Basic Data Types.cpp

---
+++ C++/Basic Data Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    printf("%.3f\n", d); 
+    printf("%.9lf\n", e);
+    
+    return 0;
+}
